0ae6758ce2e784b6ac1195ceb0db3222a7b1d5cb
zhouzhichao <zhouzhichao@qmtv.cm>
2017-05-11 14:47:33 +0800

    var

--- tests/func/StmtsTest.php
+++ tests/func/StmtsTest.php
@@ -39,7 +39,6 @@ class StmtsTest extends PHPUnit_Framework_TestCase
 
     public function test_call()
     {
-        $this->assertEquals(1,2);
         $stmtParse = new \st\parse\Stmts(__DIR__ . '/../normal.php', $this->parser);
         /*$vars = $stmtParse->getAllVars();
         $this->assertEquals('c\\h\\Y', $vars['y']->type);*/
